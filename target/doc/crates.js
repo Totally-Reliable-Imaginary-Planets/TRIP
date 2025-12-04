45569868de33252ef9e88a63968c10150748087b
whyKVD <faccincani.al@gmail.com>
2025-12-04 01:18:04 +0100

    core: Actually added docs

---
+++ target/doc/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["TRIP","common_game","lazy_static","paste"];
+//{"start":21,"fragment_lengths":[6,14,14,8]}
---
+++ target/doc/trait.impl/common_game/components/planet/trait.PlanetAI.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["TRIP",[["impl <a class=\"trait\" href=\"common_game/components/planet/trait.PlanetAI.html\" title=\"trait common_game::components::planet::PlanetAI\">PlanetAI</a> for <a class=\"struct\" href=\"TRIP/struct.AI.html\" title=\"struct TRIP::AI\">AI</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[254]}
